e0bbe76ad3d20a252df5cfcbc06f6894c56c62b9
Krishnakant Kushwaha <145730098+KRISHNAKANT-KUSHWAHA@users.noreply.github.com>
2026-06-16 20:37:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function shortcut(s1, s2) {
   // your code here
  let str=""
-	let str1=s1.char(0);
-	let str2=s2.char(0);
+	let str1=s1.charAt(0);
+	let str2=s2.charAt(0);
 	if(s1==='' || s2==='') return ''
-	else return str+=s1+s2;
+	else return str+=str1+str22;
 }
 
-// Do not change the code below.
+// // // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
